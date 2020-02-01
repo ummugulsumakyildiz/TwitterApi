8e22d5912ae599c05c9ad89897353c41b008be6f
ÜmmüGülsüm Akyıldız <gulsum.akyildizz@gmail.com>
2020-02-01 15:07:09 +0300

    TweetinviAPI projeye implement edildi

--- TwitterApi/Program.cs
+++ TwitterApi/Program.cs
@@ -9,7 +9,7 @@ namespace TwitterApi
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application.zzz
         /// </summary>
         [STAThread]
         static void Main()
