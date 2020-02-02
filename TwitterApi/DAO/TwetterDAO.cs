8a6f0345951450f376bf070725b63e784ce21917
ÜmmüGülsüm Akyıldız <gulsum.akyildizz@gmail.com>
2020-02-02 20:19:11 +0300

    Revert "Tweet Al ve Tweet Oku sekmeleri düzenlendi.Veri tabanı işlemeleri tamamlandı. mdf dosyası projeye eklendi"
    This reverts commit 2ff7b1c992719a1d8db14a0528782e1a70fc5e4c.

--- TwitterApi/DAO/TwetterDAO.cs
+++ TwitterApi/DAO/TwetterDAO.cs
@@ -1,11 +1,8 @@
-﻿using EntityFramework.Utilities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tweetinvi;
-using Tweetinvi.Models;
 
 namespace TwitterApi.DAO
 {
@@ -21,40 +18,5 @@ namespace TwitterApi.DAO
             }
             return false;
         }
-
-        public bool SearchSave(TweetPost tweets)
-        {
-            baglanti.TweetPost.Add(tweets);
-            int result = baglanti.SaveChanges();
-            if (result > 0)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public bool InsertAllPost(IEnumerable<ITweet>  tweetList ,string username,string type)
-        {
-            try
-            {
-                List<TweetPost> tweetPosts = new List<TweetPost>();
-                foreach (var item in tweetList)
-                {
-                    TweetPost post = new TweetPost();
-                    post.tweet = item.FullText;
-                    post.created_at = item.TweetLocalCreationDate;
-                    post.username = username;
-                    post.type = type;
-                    tweetPosts.Add(post);
-                }
-                EFBatchOperation.For(baglanti, baglanti.TweetPost).InsertAll(tweetPosts);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
     }
 }
